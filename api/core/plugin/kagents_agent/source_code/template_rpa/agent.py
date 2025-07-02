577e8435e2cb058fba138a67f8d3408cdf95eaea
AaronMKk <shaodwaaron@gmail.com>
2025-07-02 15:08:37 +0800

    only keep last chunk

--- api/core/plugin/kagents_agent/source_code/template_rpa/agent.py
+++ api/core/plugin/kagents_agent/source_code/template_rpa/agent.py
@@ -1,5 +1,3 @@
-
-# nosec
 import asyncio
 import os
 from collections.abc import Generator
@@ -9,8 +7,12 @@ from uuid import uuid4
 import httpx
 from a2a.client import A2AClient
 from a2a.types import (
+    Message,
     MessageSendParams,
     SendStreamingMessageRequest,
+    Task,
+    TaskState,
+    TaskStatusUpdateEvent,
 )
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
@@ -18,11 +20,28 @@ from ruamel.yaml import YAML
 
 yaml = YAML()
 
-file_exts = [".txt", ".md", ".mdx", ".markdown", ".pdf",
-             ".html", ".xlsx", ".xls", ".doc", ".docx", ".csv",
-             ".eml", ".msg", ".pptx", ".ppt", ".xml", ".epub"]
-
+file_exts = [
+    ".txt",
+    ".md",
+    ".mdx",
+    ".markdown",
+    ".pdf",
+    ".html",
+    ".xlsx",
+    ".xls",
+    ".doc",
+    ".docx",
+    ".csv",
+    ".eml",
+    ".msg",
+    ".pptx",
+    ".ppt",
+    ".xml",
+    ".epub",
+]
 response_queue: asyncio.Queue = asyncio.Queue()
+
+
 async def run(send_message_payload):
     all_responses = []
     async with httpx.AsyncClient() as httpx_client:
@@ -34,7 +53,24 @@ async def run(send_message_payload):
         )
         stream_response = client.send_message_streaming(streaming_request)
         async for chunk in stream_response:
-            await response_queue.put(str(chunk))
+            event = chunk.root.result
+            is_final_event = (
+                (isinstance(event, TaskStatusUpdateEvent) and event.final)
+                or isinstance(event, Message)
+                or (
+                    isinstance(event, Task)
+                    and event.status.state
+                    in (
+                        TaskState.completed,
+                        TaskState.canceled,
+                        TaskState.failed,
+                        TaskState.rejected,
+                        TaskState.unknown,
+                    )
+                )
+            )
+            if is_final_event:
+                await response_queue.put(chunk.model_dump())
         await response_queue.put(None)
 
     return all_responses
@@ -63,8 +99,14 @@ class agent(Tool):  # noqa: N801
             "inputs": [
                 {
                     "name": p["name"],
-                    "type": "file" if any(
-                        tool_parameters.get(p["name"], "").lower().endswith(ext) for ext in file_exts) else "text",
+                    "type": (
+                        "file"
+                        if any(
+                            tool_parameters.get(p["name"], "").lower().endswith(ext)
+                            for ext in file_exts
+                        )
+                        else "text"
+                    ),
                     "value": tool_parameters.get(p["name"]),
                     "description": p["human_description"],
                     "required": p["required"],
@@ -74,8 +116,14 @@ class agent(Tool):  # noqa: N801
             "outputs": [
                 {
                     "name": op["name"],
-                    "type": "file" if any(
-                        tool_parameters.get(op["name"], "").lower().endswith(ext) for ext in file_exts) else "text",
+                    "type": (
+                        "file"
+                        if any(
+                            tool_parameters.get(op["name"], "").lower().endswith(ext)
+                            for ext in file_exts
+                        )
+                        else "text"
+                    ),
                     "value": "",
                     "description": op["description"],
                     "required": op["required"],
@@ -96,11 +144,10 @@ class agent(Tool):  # noqa: N801
                 "acceptedOutputModes": ["text/plain", "application/json"],
             },
         }
-
         asyncio.run(run(send_message_payload))
         steaming = True
         while steaming:
             chunk = asyncio.run(response_queue.get())
             if chunk is None:
-                steaming = False
-            yield self.create_stream_variable_message("a2a_streaming_response", str(chunk) + "\n")
+                break
+            yield self.create_json_message(chunk)
